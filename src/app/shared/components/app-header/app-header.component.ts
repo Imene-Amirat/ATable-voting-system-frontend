ec3badcb88cf47c5549be1a1917263e32e075485
Imene-Amirat <imene2004.amirat@gmail.com>
2025-11-04 11:57:32 +0100

    add app header and footer

---
+++ src/app/shared/components/app-header/app-header.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject  } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-app-header',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './app-header.component.html',
+  styleUrl: './app-header.component.scss'
+})
+export class AppHeaderComponent {
+
+}
