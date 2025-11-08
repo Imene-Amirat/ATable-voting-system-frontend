b1a2cdaa1113d1508501cbba8736b92138169d36
Imene-Amirat <imene2004.amirat@gmail.com>
2025-11-08 14:04:04 +0100

    integrate app header and footer components into the main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from './shared/components/app-header/app-header.component';
+import { AppFooterComponent } from './shared/components/app-footer/app-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AppHeaderComponent, RouterOutlet, AppFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
