c6e8f8288134cbb4d5932154259101ce69e65f3e
Imene-Amirat <imene2004.amirat@gmail.com>
2025-11-08 22:23:08 +0100

    refactor: rename header and footer components

--- src/app/layout/user-layout/user-layout.component.ts
+++ src/app/layout/user-layout/user-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AppHeaderComponent } from '../../shared/components/app-header/app-header.component';
+import { AppFooterComponent } from '../../shared/components/app-footer/app-footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-layout',
   standalone: true,
-  imports: [],
+  imports: [AppHeaderComponent, AppFooterComponent, RouterOutlet],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
 })
--- src/app/shared/components/app-header/app-header.component.ts
+++ src/app/shared/components/app-header/app-header.component.ts
@@ -2,7 +2,7 @@ import { Component, inject  } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-app-header',
+  selector: 'app-header',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './app-header.component.html',
