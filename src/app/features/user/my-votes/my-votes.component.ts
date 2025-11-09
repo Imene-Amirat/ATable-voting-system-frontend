43fb64edc083d8ced04971a0e32f76537a337cb4
Imene-Amirat <imene2004.amirat@gmail.com>
2025-11-09 23:30:57 +0100

    feat: add authentication and user features

---
+++ src/app/features/user/my-votes/my-votes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-votes',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-votes.component.html',
+  styleUrl: './my-votes.component.scss'
+})
+export class MyVotesComponent {
+
+}
