31473b08ef49ceb0ab455d14b7da867d80031f03
Imene-Amirat <imene2004.amirat@gmail.com>
2025-11-09 23:30:11 +0100

    add auth and user paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,33 @@
 import { Routes } from '@angular/router';
 import { UserLayoutComponent } from './layout/user-layout/user-layout.component';
+import { HomeComponent } from './features/user/home/home.component';
+import { MenuComponent } from './features/user/menu/menu.component';
+import { ResultsComponent } from './features/user/results/results.component';
+import { MyVotesComponent } from './features/user/my-votes/my-votes.component';
+import { ProfileComponent } from './features/user/profile/profile.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
 
 export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+    ],
+  },
   {
     path: '',
     component: UserLayoutComponent,
     children: [
-      { path: '',   },
-      { path: 'menu',   },
-      { path: 'results',   },
-      { path: 'me/votes',   },
-      { path: 'me/profile',   },
-    ]
-  }
+      { path: '', component: HomeComponent },
+      { path: 'menu', component: MenuComponent },
+      { path: 'results', component: ResultsComponent },
+      { path: 'me/votes', component: MyVotesComponent },
+      { path: 'me/profile', component: ProfileComponent },
+    ],
+  },
+  { path: '**', redirectTo: '' },
 ];
